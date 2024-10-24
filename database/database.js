2ff9250c78e061c9a538869cec6cc06753c1975c
kyuuukira <genesistroy.fdc@gmail.com>
2024-10-24 10:29:33 +0800

    d

--- database/database.js
+++ database/database.js
@@ -27,9 +27,9 @@ class DatabaseConnection {
             });
         } else if (this.databaseType === 'sqlite') {
             try {
-                // const dbPath = process.env.SQLITE_FILE;
+                const dbPath = process.env.SQLITE_FILE;
                 // const dbPath = '/tmp/sessions.sqlite';
-                const dbPath = path.join(__dirname, '..', 'tmp', 'sessions.sqlite');
+                // const dbPath = path.join(__dirname, '..', 'tmp', 'sessions.sqlite');
                 this.connection = new sqlite3(dbPath);
             } catch (err) {
                 console.error('Error connecting to SQLite database:', err.message);
